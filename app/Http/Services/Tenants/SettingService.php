c4cee8fa19cc3903c0bda892cb416fac13e59e04
Abdul Saboor <Abdulsaboor6226@gmail.com>
2023-11-14 11:48:00 +0500

    setting data save and all group fetch

--- app/Http/Services/Tenants/SettingService.php
+++ app/Http/Services/Tenants/SettingService.php
@@ -4,10 +4,11 @@ namespace App\Http\Services\Tenants;
 
 use App\Helpers\Constant;
 use App\Traits\ImageUpload;
-use App\Models\Tenants\Setting;
+//use App\Models\Tenants\Setting;
 use App\Exceptions\CustomException;
 use App\Contracts\Tenants\SettingContract;
 use App\Http\Resources\Tenants\LogoResource;
+use QCod\Settings\Setting\Setting;
 
 /**
  * @var SettingService
@@ -17,22 +18,22 @@ class SettingService implements SettingContract
     public $model;
     public function __construct()
     {
-        $this->model = new Setting();
+        $this->model = settings();
     }
     public function index()
     {
         $settings = [
-            "logo" => asset($this->model::getValue(Constant::LOGO, "logo")),
-            "primary_color" => $this->model::getValue(Constant::COLOR_SCHEME, "primary"),
-            "secondary_color" => $this->model::getValue(Constant::COLOR_SCHEME, "secondary"),
-            "name" => $this->model::getValue(Constant::ORGANIZATION, "name"),
-            "phone" => $this->model::getValue(Constant::ORGANIZATION, "phone"),
-            "website" => $this->model::getValue(Constant::ORGANIZATION, "website"),
-            "candidate_reapply_days" => $this->model::getValue(Constant::CONFIGURATION, "candidate_reapply_days"),
-            "company_contract_email" => $this->model::getValue(Constant::CONFIGURATION, "company_contract_email"),
-            "default_email_signature" => $this->model::getValue(Constant::CONFIGURATION, "default_email_signature"),
-            "company_title_about" => $this->model::getValue(Constant::CONFIGURATION, "company_title_about"),
-            "job_description_about" => $this->model::getValue(Constant::CONFIGURATION, "job_description_about"),
+            "logo" => asset($this->model->group(Constant::LOGO)->get("logo")),
+            "primary_color" => $this->model->group(Constant::COLOR_SCHEME)->get("primary"),
+            "secondary_color" => $this->model->group(Constant::COLOR_SCHEME)->get("secondary"),
+            "name" => $this->model->group(Constant::ORGANIZATION)->get("name"),
+            "phone" => $this->model->group(Constant::ORGANIZATION)->get("phone"),
+            "website" => $this->model->group(Constant::ORGANIZATION)->get("website"),
+            "candidate_reapply_days" => $this->model->group(Constant::CONFIGURATION)->get("candidate_reapply_days"),
+            "company_contract_email" => $this->model->group(Constant::CONFIGURATION)->get("company_contract_email"),
+            "default_email_signature" => $this->model->group(Constant::CONFIGURATION)->get("default_email_signature"),
+            "company_title_about" => $this->model->group(Constant::CONFIGURATION)->get("company_title_about"),
+            "job_description_about" => $this->model->group(Constant::CONFIGURATION)->get("job_description_about"),
         ];
         return $settings;
     }
@@ -46,36 +47,36 @@ class SettingService implements SettingContract
     {
         switch ($type) {
             case ('logo'):
-                // $model = $model->group('logo')->set('logo', $data['logo']);
+                 $model = $model->group('logo')->set('logo', $data['logo']);
                 break;
             case ('color-scheme'):
-                // $model = $model->group('color-scheme')->set([
-                //     'primary' => $data['primary'],
-                //     'secondary' => $data['secondary'],
-                // ]);
+                 $model = $model->group('color-scheme')->set([
+                     'primary' => $data['primary'],
+                     'secondary' => $data['secondary'],
+                 ]);
                 break;
             case ('organization'):
-                // $model = $model->group('organization')->set([
-                //     'name' => $data['name'],
-                //     'phone' => $data['phone'],
-                //     'website' => $data['website'],
-                // ]);
+                 $model = $model->group('organization')->set([
+                     'name' => $data['name'],
+                     'phone' => $data['phone'],
+                     'website' => $data['website'],
+                 ]);
                 break;
             case ('configuration'):
-                // $model = $model->group('configuration')->set([
-                //     'candidate_reapply_days' => $data['candidate_reapply_days'],
-                //     'company_contract_email' => $data['company_contract_email'],
-                //     'default_email_signature' => $data['default_email_signature'],
-                //     'company_title_about' => $data['company_title_about'],
-                //     'job_description_about' => $data['job_description_about'],
-                // ]);
+                 $model = $model->group('configuration')->set([
+                     'candidate_reapply_days' => $data['candidate_reapply_days'],
+                     'company_contract_email' => $data['company_contract_email'],
+                     'default_email_signature' => $data['default_email_signature'],
+                     'company_title_about' => $data['company_title_about'],
+                     'job_description_about' => $data['job_description_about'],
+                 ]);
                 break;
             case ('core-value'):
-                // $model = $model->group('core-value')->set([
-                //     'title' => $data['title'],
-                //     'icon' => $data['icon'],
-                //     'description' => $data['description'],
-                // ]);
+                 $model = $model->group('core-value')->set([
+                     'title' => $data['title'],
+                     'icon' => $data['icon'],
+                     'description' => $data['description'],
+                 ]);
                 break;
             default:
         }
