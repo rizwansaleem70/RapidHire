ca854e4cc620f6efc71cddada3bbd6010ca8a54e
Abdul Saboor <Abdulsaboor6226@gmail.com>
2023-11-14 14:17:17 +0500

    none

--- app/Http/Resources/Tenants/Job.php
+++ app/Http/Resources/Tenants/Job.php
@@ -18,18 +18,24 @@ class Job extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'country_id' => $this->country_id,
+            'state_id' => $this->state_id,
+            'city_id' => $this->city_id,
+            'department_id' => $this->department_id,
             'slug' => $this->slug,
+            'job_description' => $this->job_description,
             'type' => $this->type,
             'job_type' => $this->job_type,
             'min_salary' => $this->min_salary,
             'max_salary' => $this->max_salary,
             'post_date' => $this->post_date,
             'expiry_date' => $this->expiry_date,
+            'total_position' => $this->total_position,
             'is_active' => $this->is_active,
             'rating' => $this->rating,
             'status' => $this->status,
             'salary_deliver' => $this->salary_deliver,
-            'image' => $this->image ? asset($this->image):"",
+            'cover_image' => $this->cover_image ? asset($this->cover_image):"",
         ];
     }
 }
