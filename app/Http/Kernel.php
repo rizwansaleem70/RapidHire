ec8e061c3662e9e0cba6db2c19d92a15d25af779
Abdul Saboor <Abdulsaboor6226@gmail.com>
2023-11-17 15:11:08 +0500

    add middleware in kernal

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Cors;
 use App\Http\Middleware\IdentifyTenancy;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -43,6 +44,7 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            Cors::class
         ],
     ];
 
