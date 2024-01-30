662d8a699a12ae7d513e009aff8f68d0755013f7
Abdul Saboor <abdulsaboor6226@gmail.com>
2024-01-30 21:52:18 +0900

    user not login if not verified

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -19,7 +21,9 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers {
+        logout as performLogout;
+    }
 
     /**
      * Where to redirect candidates after login.
@@ -36,5 +40,19 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    protected function authenticated(Request $request, $user)
+    {
+        if ($user->email_verified_at != null){
+            return redirect()->route('candidate.home')->with('success', 'Logged in Successfully');
+        }else{
+            Auth::logout();
+            return redirect()->route('candidate.home')->with('message', 'Please verify first.A fresh verification link has been sent to your email address.');
+        }
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect()->route('candidate.home')->with('success','Logged out Successfully');
     }
 }
